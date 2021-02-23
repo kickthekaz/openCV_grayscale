182bbf665fd8963b9f768b52603c4a06e1339804
kazuhiro nakajima <42648221+kickthekaz@users.noreply.github.com>
2021-02-23 21:41:11 +0900

    update

--- main.py
+++ main.py
@@ -13,42 +13,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-  movies = [ filename for filename in listdir('./static/data') if not filename.startswith('.') ]
+  movies = [ filename for filename in listdir('./static/out') if not filename.startswith('.') ]
 
   return render_template("index.html", movies=movies)
-
-@app.route('/video_feed')
-def video_feed():
-  video = cv2.VideoCapture('baby.mov')
-  return Response(gen(video), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def gen(video):
-
-  while True:
-    success, image = video.read()
-    ret, jpeg = cv2.imencode('.jpg', image)
-    frame = jpeg.tobytes()
-    yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 
 @app.route('/download', methods=['POST'])
 def download():
   mv =  request.form["movie"]
-  DLfile = './static/data/'+ mv
+  DLfile = './static/out/'+ mv
   return send_file(DLfile, as_attachment=True)
 
 @app.route('/upload', methods=['POST'])
 def upload():
 
   fs = request.files['file']
-  date = datetime.now().strftime("%y%m%d%H%M%S")
-  video_path = os.path.join('./static/data/', fs.filename.split('.', 1)[0] + date + ".mov")
-  fs.save(video_path)
+  date = datetime.now().strftime("%y_%m_%d_%H_%M_%S")
+  in_path = os.path.join('./static/in/', fs.filename.split('.', 1)[0] + date + ".mov")
+  out_path = os.path.join('./static/out/', fs.filename.split('.', 1)[0] + date + ".mov")
+  fs.save(in_path)
 
-  time.sleep(3)
-
-  cap = cv2.VideoCapture(video_path)
+  cap = cv2.VideoCapture(in_path)
 
   width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
   height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -56,7 +40,7 @@ def upload():
   fps = int(cap.get(cv2.CAP_PROP_FPS))
 
   fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-  out = cv2.VideoWriter(video_path, fourcc, fps, (width, height), isColor=False)
+  out = cv2.VideoWriter(out_path, fourcc, fps, (width, height), isColor=False)
 
   for x in range(1, count):
     ret, frame = cap.read()
